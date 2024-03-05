9ff378b193bfd1889cc15c214f3da3370dc67ed3
luxe <74306678+cryptocoder-cloud@users.noreply.github.com>
2024-03-05 12:22:00 +0300

    v1.0

---
+++ main.py
@@ -0,0 +1,160 @@
+"""
+Модуль содержит функции для управления заметками.
+
+Автор: cryptocoding
+Дата создания: 03.03.2024
+"""
+import os
+import sys
+
+from modules.database import Manager
+
+manager = Manager()
+
+
+def clear_console():
+    '''
+    Функция для очистки консоли.
+    '''
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def close_script():
+    '''
+    Функция для завершения скрипта.
+    '''
+    sys.exit()
+
+
+def main():
+    '''
+    Функция для основного меню программы.
+    '''
+    clear_console()
+    print('''Выбирете действие:
+1. Добавить заметку
+2. Поиск заметок
+3. Список всех заметок
+
+4. Выйти
+''')
+
+    while True:
+        try:
+            answer = int(input('>>>'))
+            if answer < 1 or answer > 4:
+                raise ValueError
+            break  # Выход из цикла при корректном вводе
+        except ValueError:
+            print('Укажите правильное значение.')
+
+    function_dict = {
+        1: add_note,
+        2: search_notes,
+        3: find_all_notes,
+        4: close_script,
+    }
+
+    choose_function = function_dict.get(answer)
+    if choose_function:
+        choose_function()
+    else:
+        print('Неправильный выбор. Повторите попытку.')
+
+
+def add_note():
+    '''
+    Функция для добавления новой заметки.
+    '''
+    clear_console()
+    title = input('Укажите заголовок: ')
+    text = input('Укажите содержание заметки:\n')
+    if not title or not text:
+        return main()
+
+    print(manager.add_note(title, text))
+
+    return main()
+
+
+def search_notes():
+    '''
+    Функция для поиска и просмотра заметок по ключевому слову.
+    '''
+    clear_console()
+    keyword = input('Укажите ключевое слово: ')
+    search_results = manager.search_notes(keyword)
+
+    if search_results:
+        print(f"Результаты поиска по ключевому слову '{keyword}':\n")
+        for note_id, note_title in search_results:
+            print(f"{note_id}. Заголовок: {note_title}")
+
+        while True:
+            number_notes = input('\nУкажите номер заметки для просмотра содержания, 0 для выхода в меню\n>>>')
+            if number_notes == '0':
+                return main()
+
+            try:
+                number = int(number_notes)
+                text = manager.get_info_notes(number)
+                clear_console()
+                try:
+                    print(text[0])
+                except TypeError:
+                    print('Неправильный выбор. Повторите попытку.')
+                    return main()
+
+                answer = input('\n1. Удалить заметку\n2. Вернуться в меню\n>>>')
+                if answer == '1':
+                    manager.delete_note(number)
+                return main()
+            except ValueError:
+                print('Укажите правильное значение.')
+    else:
+        input('По данному ключевому слову заметок не найдено.\nНажмите любую клавишу для продолжения...')
+        return main()
+
+
+def find_all_notes():
+    '''
+    Функция для отображения всех заметок и просмотра содержания определенной заметки.
+    '''
+    clear_console()
+    all_notes = manager.get_all_notes()
+
+    for note_id, note_title in all_notes:
+        print(f"{note_id}. {note_title}")
+
+    while True:
+        number_notes = input('\nУкажите номер заметки для просмотра содержания, 0 для выхода в меню\n>>>')
+        if number_notes == '0':
+            return main()
+
+        try:
+            number = int(number_notes)
+            text = manager.get_info_notes(number)
+            clear_console()
+            try:
+                print(text[0])
+            except TypeError:
+                print('Неправильный выбор. Повторите попытку.')
+                return main()
+
+            while True:
+                answer = input('\n1. Удалить заметку\n2. Вернуться в меню\n>>>')
+                if answer in ['1', '2']:
+                    break
+                else:
+                    print('Укажите правильное значение.')
+
+            if answer == '1':
+                manager.delete_note(number)
+            return main()
+        except ValueError:
+            print('Укажите правильное значение.')
+            return main()
+
+
+if __name__ == "__main__":
+    main()
---
+++ modules/database.py
@@ -0,0 +1,73 @@
+"""
+Модуль Manager содержит класс для управления заметками в базе данных.
+
+Автор: cryptocoding
+Дата создания: 03.03.2024
+"""
+import sqlite3
+
+
+class Manager:
+    '''
+    Класс для управления заметками в базе данных.
+    '''
+    def __init__(self):
+        db_path = r'db\database.db'
+        self.conn = sqlite3.connect(db_path)
+        self.cursor = self.conn.cursor()
+        if not self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='notes'").fetchone():
+            self.create_table()
+
+    def create_table(self):
+        '''
+        Метод для создания таблицы "notes" в базе данных SQLite.
+        '''
+        self.cursor.execute('''CREATE TABLE notes (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                title TEXT NOT NULL,
+                                text TEXT NOT NULL
+                            )''')
+        self.conn.commit()
+
+    def add_note(self, title, text):
+        '''
+        Метод для добавления новой заметки в базу данных.
+        :param title: Заголовок заметки
+        :param text: Содержание заметки
+        '''
+        self.cursor.execute('INSERT INTO notes (title, text) VALUES (?, ?)', (title, text))
+        self.conn.commit()
+        return 'Заметка успешно добавлена!'
+
+    def get_all_notes(self):
+        '''
+        Метод для добавления новой заметки в базу данных.
+
+        Параметры:
+        title (str): Заголовок заметки.
+        text (str): Содержание заметки.
+        '''
+        self.cursor.execute("SELECT id, title FROM notes")
+        return self.cursor.fetchall()
+
+    def get_info_notes(self, note_id):
+        '''
+        Метод для получения содержания заметки по её ID.
+        Возвращает текст заметки или None, если заметка не найдена.
+        '''
+        self.cursor.execute("SELECT text FROM notes WHERE id = ?", (note_id,))
+        return self.cursor.fetchone()
+
+    def delete_note(self, note_id):
+        '''
+        Метод для удаления заметки по её ID из базы данных.
+        '''
+        self.cursor.execute("DELETE FROM notes WHERE id=?", (note_id,))
+        self.conn.commit()
+
+    def search_notes(self, keyword):
+        '''
+        Метод для удаления заметки по её ID.
+        '''
+        self.cursor.execute("SELECT id, title FROM notes WHERE text LIKE ?", ('%'+keyword+'%',))
+        return self.cursor.fetchall()
